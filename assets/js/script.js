19ba630566c3fa110c21cba460c89111aef9d455
c1ntiia <scintia280@gmail.com>
2026-06-18 22:36:43 -0300

    style: pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 const acoes = [
-    { icone: '🚮', texto: 'Não misture metais com lixo orgânico. O aço é 100% reciclável, mas ele precisa chegar limpo às cooperativas para facilitar a triagem magnética.'},
-    { icone: '♻️', texto: 'Muitas embalagens de aço possuem o símbolo de reciclagem. Dar preferência a produtos com embalagens metálicas é uma escolha consciente, já que o aço volta para a prateleira em cerca de 60 dias.'},
-    { icone: '🌀', texto: 'Ao trocar de eletrodomésticos, procure empresas ou prefeituras que ofereçam o serviço de coleta de "Linha Branca". Esses produtos são ricos em aço de alta qualidade.'},
-    { icone: '🏗️', texto: 'Se for construir ou reformar, considere o uso de estruturas metálicas. Além de gerarem menos resíduos no canteiro de obras, o aço utilizado pode ser reaproveitado integralmente no futuro.'}
+    { icone: '🚮', texto: 'Descarte Correto: Não misture metais com lixo orgânico. O aço é 100% reciclável, mas ele precisa chegar limpo às cooperativas para facilitar a triagem magnética.'},
+    { icone: '♻️', texto: 'Identifique o Selo de Reciclabilidade: Muitas embalagens de aço possuem o símbolo de reciclagem. Dar preferência a produtos com embalagens metálicas é uma escolha consciente, já que o aço volta para a prateleira em cerca de 60 dias.'},
+    { icone: '🌀', texto: 'Apoie a Logística Reversa: Ao trocar de eletrodomésticos, procure empresas ou prefeituras que ofereçam o serviço de coleta de "Linha Branca". Esses produtos são ricos em aço de alta qualidade.'},
+    { icone: '🏗️', texto: 'Construção Sustentável: Se for construir ou reformar, considere o uso de estruturas metálicas. Além de gerarem menos resíduos no canteiro de obras, o aço utilizado pode ser reaproveitado integralmente no futuro.'}
 ]
 
 // renderização dinamica
@@ -26,5 +26,22 @@ window.addEventListener('scroll', () => {
         nav.computedStyleMap.display = 'none'
     }else {
         nav.computedStyleMap.display = 'block'
+    }
+})
+
+const form = document.getElementById("formulario-adesao")
+const msg = document.getElementById("mensagem-retorno")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefaut()
+
+    if(form.checkValidity()) {
+        const nome = document.getElementById("nome").value
+        msg.textContent = `✅ Sucesso, ${nome}! Adesão registrada.`
+        msg.className = 'mensagem-retorno sucesso'
+        form.reset()
+    }else {
+        msg.textContent = '❌ Preencha os campos corretamente.'
+        msg.className = 'mensagem-retorno erro'
     }
 })
